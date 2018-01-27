acaa99ab1a2fa20bc3dc80162f75530a5a862e9c
Keith Mendoza <pantherse@gmail.com>
2018-01-27 07:19:33 -0800

    Modify copyright line

--- include/HandlerError.h
+++ include/HandlerError.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Keith Mendoza
+ * Copyright 2017-present Keith Mendoza
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
